bea6534abb4c1c84f4b6a3110db98c64a5f511e1
Avichai Shapira <avichai.shapira@sealights.io>
2021-11-03 22:00:10 +0200

    added models

--- bekami/bekami/Data/bekamiContext.cs
+++ bekami/bekami/Data/bekamiContext.cs
@@ -15,9 +15,13 @@ namespace bekami.Data
         }
 
         public DbSet<bekami.Models.Product> Product { get; set; }
+
         public DbSet<bekami.Models.Category> Category { get; set; }
         public DbSet<bekami.Models.Tags> Tags { get; set; }
         public DbSet<bekami.Models.Color> Color { get; set; }
         public DbSet<bekami.Models.Branch> Branch { get; set; }
+        public DbSet<bekami.Models.OrderProduct> OrderProduct { get; set; }
+        public DbSet<bekami.Models.Order> Order { get; set; }
+
     }
 }
---
+++ bekami/bekami/Models/User.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bekami.Models
+{
+    public enum Role
+    {
+        Admin,
+        Customer
+    }
+
+    public class User
+    {
+
+        /// <--------------------->
+        /// Account Details
+        /// <--------------------->
+        [DisplayName("ID")]
+        public int UserId { get; set; }
+
+        [Required]
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The passwords you entered do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public Role Type { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Registration date")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime Created { get; set; }
+        
+
+        [DisplayName("Your orders")]
+        public ICollection<Order> Orders { get; set; }
+        
+    }
+}
