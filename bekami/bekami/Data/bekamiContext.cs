c5973d45298341085d44314954a8b2177c40bfa9
Avichai Shapira <avichai.shapira@sealights.io>
2021-11-11 00:31:54 +0200

    merge from bella

--- bekami/bekami/Data/bekamiContext.cs
+++ bekami/bekami/Data/bekamiContext.cs
@@ -21,7 +21,6 @@ namespace bekami.Data
         public DbSet<bekami.Models.Category> Category { get; set; }
         public DbSet<bekami.Models.Tags> Tags { get; set; }
         public DbSet<bekami.Models.Color> Color { get; set; }
-        public DbSet<bekami.Models.Branch> Branch { get; set; }
         public DbSet<bekami.Models.OrderProduct> OrderProduct { get; set; }
         public DbSet<bekami.Models.Order> Order { get; set; }
         
